0beb8c604345d96d398750d8c1d3dac6d9d84820
elleromero <elleromero@users.noreply.github.com>
2023-12-07 21:59:59 +0800

    updated headerbar

--- components/shared/Headerbar.tsx
+++ components/shared/Headerbar.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+
 export default function Headerbar() {
   return (
     <header className="bg-red-500">
-      <span>logo</span>
-      <nav>controls</nav>
+      <div className="container mx-auto p-4 flex items-center justify-between">
+        <div className="logo flex items-center">
+          <Image
+            width={34}
+            height={34}
+            src="/assets/quill.png"
+            alt="quill-logo"
+            className="mr-4"
+          />
+          <span>quill</span>
+        </div>
+        <nav>controls</nav>
+      </div>
     </header>
   );
 }
